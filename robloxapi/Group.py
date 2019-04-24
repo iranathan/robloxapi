690f7c018912debd188b8e200bd7a71524331b4b
ira <38579267+iranathan@users.noreply.github.com>
2019-04-24 15:04:58 +0200

    Create Group.py

---
+++ robloxapi/Group.py
@@ -0,0 +1,51 @@
+from .request import Request
+from bs4 import BeautifulSoup
+import requests
+from .xcsrf import get_xcsrf
+import json
+class Group:
+     
+    def __init__(self, cookie=str(), id=str()):
+        if cookie is None:
+            self.cookie = False
+            self.id = False
+        self.cookie = cookie
+        self.id = id
+        self.xcsrf = get_xcsrf()
+    
+    def groupSearch(name, show):
+        url = f'https://www.roblox.com/search/groups/list-json?keyword={name}&maxRows={show}&startRow=0'
+        r = requests.get(url)
+        r = json.loads(r.text)
+        results = r['GroupSearchResults']        
+        return results
+
+    def getGroup(id, login=False):
+        url = f'https://groups.roblox.com/v1/groups/{id}'
+        r = requests.get(url)
+        if r.status_code is not 200:
+            return {'found': False}
+        else:
+            groupinfo = json.loads(r.text)
+            groupinfo['found'] = True
+            return groupinfo
+    
+    def getGroupRoles(id, login=False):
+        url = f'https://groups.roblox.com/v1/groups/{id}/roles'
+        if login is True and self.cookie is not False:
+            cookies = {
+                '.ROBLOSECURITY': self.cookie
+            }
+        else:
+            cookies = {}
+        r = requests.get(url, cookies=cookies)
+        if r.status_code is not 200:
+            return {'found': False}
+        else:
+            data = json.loads(r.text)
+            data['found'] = True
+            del data['groupId']
+            return data
+        
+
+
