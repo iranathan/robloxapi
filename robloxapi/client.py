1ab401fbfaf70321201a99f617fb1d810afe7a12
ira <38579267+iranathan@users.noreply.github.com>
2019-04-24 12:53:58 +0200

    Update client.py

--- robloxapi/client.py
+++ robloxapi/client.py
@@ -7,7 +7,7 @@ def client(cookie=str()):
             '.ROBLOSECURITY': cookie
         }
         r = requests.get('https://www.roblox.com/game/GetCurrentUser.ashx', cookies=cookies)
-        if r is 'null':
+        if r.text is 'null':
             print('Rython: Failed to login. Using Rython without login')
             functions = lambda: None
             functions.User = User()
