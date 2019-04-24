b625e91967e9ff73867f9692eeea78394f35a14c
ira <38579267+iranathan@users.noreply.github.com>
2019-04-24 18:26:06 +0200

    Update Group.py

--- robloxapi/Group.py
+++ robloxapi/Group.py
@@ -13,8 +13,8 @@ class Group:
         self.id = id
         self.xcsrf = get_xcsrf()
     
-    def groupSearch(self, groupName, show):
-        url = f'https://www.roblox.com/search/groups/list-json?keyword={groupName}&maxRows={str(show)}&startRow=0'
+    def groupSearch(self, name, show):
+        url = f'https://www.roblox.com/search/groups/list-json?keyword={name}&maxRows={show}&startRow=0'
         r = requests.get(url)
         r = json.loads(r.text)
         results = r['GroupSearchResults']        
@@ -24,7 +24,6 @@ class Group:
         url = f'https://groups.roblox.com/v1/groups/{id}'
         r = requests.get(url)
         if r.status_code is not 200:
-            print(r.status_code)
             return {'found': False}
         else:
             groupinfo = json.loads(r.text)
@@ -47,6 +46,33 @@ class Group:
             data['found'] = True
             del data['groupId']
             return data
+
+    def groupPayout(self, groupid, id, amount):
+        url = f'https://www.roblox.com/groups/{groupid}/one-time-payout/false'
+        if self.cookie is not False:
+            data = {
+                'percentages': {id: amount}
+            }
+            headers = {
+                'X-CSRF-TOKEN': get_xcsrf(),
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:66.0) Gecko/20100101 Firefox/66.0'
+            }
+            data = json.loads(data)
+            r = requests.post(url, data=data, headers=headers)
+            if r.status_code == 403:
+                headers['X-CSRF-TOKEN'] = r.headers['X-CSRF-TOKEN']
+                r2 = requests.post(url, data=data, headers=headers)
+                if r2 == 200:
+                    return True
+                else:
+                    return False
+            elif r.status_code == 200:
+                return True
+            else:
+                return False
+        else:
+            raise Exception('Not logged in..')
+
         
 
 
