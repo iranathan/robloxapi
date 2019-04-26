9d9a2cb7335ffccc8f6e8f517726db5584d5db56
ira <38579267+iranathan@users.noreply.github.com>
2019-04-26 16:46:02 +0200

    Update User.py

--- robloxapi/User.py
+++ robloxapi/User.py
@@ -1,4 +1,4 @@
-from .request import Request
+from .request import request
 from bs4 import BeautifulSoup
 import requests
 from .xcsrf import get_xcsrf
@@ -6,31 +6,28 @@ import json
 class User:
      
     def __init__(self, cookie=str(), id=str()):
-        if cookie is None:
-            self.cookie = False
-            self.id = False
-        self.cookie = cookie
-        self.id = id
+        self.cookie = None
+        self.request = request(cookie)
         self.xcsrf = get_xcsrf()
     
     #/users/get-by-username?username={id}
     def IdByUsername(self, username):
-        r = Request('http://api.roblox.com/users/get-by-username?username=' + username, parse=True)
-        return r
+        r = self.request(url='http://api.roblox.com/users/get-by-username?username=' + username)
+        return json.loads(r)
     #/users/{id}
     def UsernameById(self, id):
-        r = Request('http://api.roblox.com/users/' + id, parse=True)
+        r = self.request(url='http://api.roblox.com/users/' + id)
         return r
     
     
     def getProfile(self, id):
         url = 'https://www.roblox.com/users/' + str(id) + '/profile'
-        r = Request(url, parse=False)
+        r = self.request(url=url)
         soup = BeautifulSoup(r, 'html.parser')
         username = soup.find('h2').getText()
         avatar = str(soup.find('img').get('src'))
         blurb = soup.find('span', {'class': 'profile-about-content-text linkify'}).getText()
-        status_req = requests.get('https://www.roblox.com/users/profile/profileheader-json?userId=' + id)
+        status_req = self.request(url='https://www.roblox.com/users/profile/profileheader-json?userId=' + id)
         data = json.loads(status_req.text)
         status = data['UserStatus']
         follow_count = data['FollowersCount']
@@ -85,139 +82,29 @@ class User:
 
     #https://www.roblox.com/messages/send
     def send_message(self, receiver_id, subject, body):
-        if self.cookie is False:
-            raise Exception('Not logged in.')
-            
         data = {
             'body': body,
             'recipientid': receiver_id,
             'subject': subject
         }
-        cookies = {
-            '.ROBLOSECURITY': self.cookie
-        }
-        headers = {
-            'X-CSRF-TOKEN': get_xcsrf(),
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:66.0) Gecko/20100101 Firefox/66.0'
-        }
-        r = requests.post('https://www.roblox.com/messages/send', data=data, cookies=cookies, headers=headers)
-        print(r.headers['X-CSRF-TOKEN'])
-        headers['X-CSRF-TOKEN'] = r.headers['X-CSRF-TOKEN']
-        res = requests.post('https://www.roblox.com/messages/send', data=data, cookies=cookies, headers=headers)
-        return res.text + ' ' + str(res.status_code)
+
+        r = self.request(method='POST', url='https://www.roblox.com/messages/send', data=data)
+        return json.loads(res.text)
 
     def block_user(self, id):
-        if self.cookie is False:
-            raise Exception('Not logged in.')
         url = 'https://www.roblox.com/userblock/blockuser'
         data = {
             'blockeeId': id
         }
-        cookies = {
-            '.ROBLOSECURITY': self.cookie
-        }
-        headers = {
-            'X-CSRF-TOKEN': get_xcsrf(),
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:66.0) Gecko/20100101 Firefox/66.0'
-        }
-        res = requests.post(url, data=data, cookies=cookies, headers=headers)
-        if res.status_code == 403:
-            headers['X-CSRF-TOKEN'] = res.headers['X-CSRF-TOKEN']
-            res = requests.post(url, data=data, cookies=cookies, headers=headers)
-            if json.loads(res.text)['success'] is True:
-                return json.loads(res.text)
-        elif res.status_code != 403 and res.status_code != 200:
-            print('Rython: Failed to block user')
-            return False
-        else:
-            if json.loads(res.text)['success'] is True:
-                return json.loads(res.text)
-            else:
-                print('Rython: Failed to block user')
-                return False
+        res = self.request(url=url, data=data)
+        return json.loads(res)
 
     #https://www.roblox.com/userblock/unblockuser
     def unblock_user(self, id):
-        if self.cookie is False:
-            raise Exception('Not logged in.')
         url = 'https://www.roblox.com/userblock/unblockuser'
         data = {
             'blockeeId': id
         }
-        cookies = {
-            '.ROBLOSECURITY': self.cookie
-        }
-        headers = {
-            'X-CSRF-TOKEN': get_xcsrf(),
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:66.0) Gecko/20100101 Firefox/66.0'
-        }
-        res = requests.post(url, data=data, cookies=cookies, headers=headers)
-        if res.status_code == 403:
-            headers['X-CSRF-TOKEN'] = res.headers['X-CSRF-TOKEN']
-            r = requests.post(url, data=data, cookies=cookies, headers=headers)
-            if json.loads(r.text)['success'] is True:
-                return json.loads(r.text)
-        elif r.status_code != 403 and r.status_code != 200:
-            print('Rython: Failed to block user')
-            return False
-        else:
-            if json.loads(r.text)['success'] is True:
-                return json.loads(r.text)
-            else:
-                print('Rython: Failed to block user')
-                return False
-
-    def reportUser(self, id, tag, message):
-        if self.cookie is not False:
-            url = f'https://web.roblox.com/abusereport/userprofile?id={str(id)}&redirecturl=https%3a%2f%2fweb.roblox.com%2fusers%2f{str(id)}%2fprofile'
-            cookies = {
-                '.ROBLOSECURITY': self.cookie
-            }
-            headers = {
-                'X-CSRF-TOKEN': get_xcsrf(),
-                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:66.0) Gecko/20100101 Firefox/66.0'
-            }
-            data = {
-                'id': id,
-                'RedirectUrl': f'https://web.roblox.com/users/{str(id)}/profile',
-                'Comment': message,
-                'PartyGuid': '',
-                'ConversationId': '', #what does roblox want with those 2 things lol
-                'ReportCategory': int(tag)
-            }
-            data = json.dumps(data)
-            r = requests.post(url, data=data, headers=headers, cookies=cookies)
-            return r.status_code
-            #1 = Inappropriate Language - Profanity & Adult Content
-            #2 = Asking for or Giving Priver Information
-            #3 = Bullying, Harassment, Hate Speech
-            #4 = Dating (wtf)
-            #5 = Exploiting, Cheating, Spamming
-            #6 = Account Theft - Phishing, Hacking, Trading
-            #7 = Inappropriate Content - Place, Image, Model
-            #8 = Real Life Threats & Suicide Threats
-            #9 = Other Rule violation
-
-        
-             
-            
-            
-        
-    
-        
-
-
-        
-        
-
-
-
-
-    
-
-    
-
-
-
-        
-
+        res = self.request(url=url, data=data)
+        return json.loads(res)
+   
