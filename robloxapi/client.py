f725ee33e5b3eab74133ba91a6643dc9a6f19b40
ira <38579267+iranathan@users.noreply.github.com>
2019-04-26 16:49:31 +0200

    Update client.py

--- robloxapi/client.py
+++ robloxapi/client.py
@@ -3,9 +3,9 @@ from .User import User
 from .Group import Group
 def client(cookie=str()):
     global functions
-        r = requests.get('https://www.roblox.com/game/GetCurrentUser.ashx', cookies=cookies)
-            functions = lambda: None
-            functions.User = User(cookie, False)
-            functions.Group = Group(cookie, False)
-            return functions
+    r = requests.get('https://www.roblox.com/game/GetCurrentUser.ashx', cookies=cookies)
+    functions = lambda: None
+    functions.User = User(cookie, False)
+    functions.Group = Group(cookie, False)
+    return functions
        
