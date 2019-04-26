38377f106b18c451dbdb161fdc3fce08f24220d7
ira <38579267+iranathan@users.noreply.github.com>
2019-04-26 16:46:24 +0200

    Update client.py

--- robloxapi/client.py
+++ robloxapi/client.py
@@ -3,27 +3,9 @@ from .User import User
 from .Group import Group
 def client(cookie=str()):
     global functions
-    if cookie:
-        cookies = {
-            '.ROBLOSECURITY': cookie
-        }
         r = requests.get('https://www.roblox.com/game/GetCurrentUser.ashx', cookies=cookies)
-        if str(r.text) == 'null':
-            print('Rython: Failed to login. Using Rython without login')
             functions = lambda: None
-            functions.User = User(False, False)
-            functions.Group = Group(False, False)
+            functions.User = User(cookie, False)
+            functions.Group = Group(cookie, False)
             return functions
-        else:   
-            functions = lambda: None
-            functions.User = User(cookie, r)
-            functions.Group = Group(cookie, r)
-            return functions
-    else:
-        functions = lambda: None
-        functions.User = User(False, False)
-        functions.Group = Group(False, False)
-        return functions
-
-
-
+       
