05d54b747262cfb8bd3aa09188043e5632dd6976
ira <38579267+iranathan@users.noreply.github.com>
2019-04-27 12:10:14 +0200

    Update Group.py

--- robloxapi/Group.py
+++ robloxapi/Group.py
@@ -5,48 +5,38 @@ from .xcsrf import get_xcsrf
 import json
 class Group:
      
-    def __init__(self, cookie=str(), id=str()):
-        if cookie is None:
-            self.cookie = False
-            self.id = False
-        self.cookie = cookie
-        self.id = id
-        self.xcsrf = get_xcsrf()
+    def __init__(self, request_client:
+        self._request = request_client.request
+        
     
     def groupSearch(self, name, show):
         url = f'https://www.roblox.com/search/groups/list-json?keyword={name}&maxRows={show}&startRow=0'
-        r = requests.get(url)
-        r = json.loads(r.text)
-        results = r['GroupSearchResults']        
+        results = json.loads(self._request(url=url, method='GET'))['GroupSearchResults']  
         return results
 
     def getGroup(self, id, login=False):
         url = f'https://groups.roblox.com/v1/groups/{id}'
-        r = requests.get(url)
-        if r.status_code is not 200:
-            return {'found': False}
-        else:
-            groupinfo = json.loads(r.text)
-            groupinfo['found'] = True
-            return groupinfo
+        results = json.loads(self._request(url=url, method='GET'))
     
     def getGroupRoles(self, id, login=False):
         url = f'https://groups.roblox.com/v1/groups/{id}/roles'
-        if login is True and self.cookie is not False:
-            cookies = {
-                '.ROBLOSECURITY': self.cookie
+        results = json.loads(self._request(url=url, method="GET"))
+    
+   def groupPayout(self, groupid, userid, amount):
+        url = f'https://groups.roblox.com/v1/groups/{groupid}/payouts'
+        data = {
+            'PayoutType': 'FixedAmount',
+            'Recipients': [
+                    {
+                        'recipientId': id,
+                        'recipientType': 'User',
+                        'amount': amount,
+                    }
+                ]
             }
-        else:
-            cookies = {}
-        r = requests.get(url, cookies=cookies)
-        if r.status_code is not 200:
-            return {'found': False}
-        else:
-            data = json.loads(r.text)
-            data['found'] = True
-            del data['groupId']
-            return data
-
+        results = json.loads(self._request(url=url, method='POST', data=data))
+        return results
+      
 
         
 
