c5d73ebeea8e5c124df3d513a9ea15a6697ca519
ira <38579267+iranathan@users.noreply.github.com>
2019-04-27 11:56:30 +0200

    Update client.py

--- robloxapi/client.py
+++ robloxapi/client.py
@@ -1,11 +1,11 @@
 from .request import request
 from .User import User
-from .Group import Group
+#from .Group import Group
 def client(cookie=str()):
     global functions
     functions = lambda: None
     functions.User = User(request(cookie))
-    functions.Group = Group(request(cookie))
+    #functions.Group = Group(request(cookie))
     return functions
         
 
