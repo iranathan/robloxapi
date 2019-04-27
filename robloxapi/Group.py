fbb51a54975e6d7de860562ef883cbecf9fec0a6
ira <38579267+iranathan@users.noreply.github.com>
2019-04-27 12:34:41 +0200

    Update Group.py

--- robloxapi/Group.py
+++ robloxapi/Group.py
@@ -9,22 +9,22 @@ class Group:
         
     
     def groupSearch(self, name, show):
-        url = f'https://www.roblox.com/search/groups/list-json?keyword={name}&maxRows={show}&startRow=0'
+        url = f'https://www.roblox.com/search/groups/list-json?keyword={str(name)}&maxRows={str(show)}&startRow=0'
         results = json.loads(self._request(url=url, method='GET'))['GroupSearchResults']  
         return results
 
     def getGroup(self, id, login=False):
-        url = f'https://groups.roblox.com/v1/groups/{id}'
+        url = f'https://groups.roblox.com/v1/groups/{str(id)}'
         results = json.loads(self._request(url=url, method='GET'))
         return results
     
     def getGroupRoles(self, id, login=False):
-        url = f'https://groups.roblox.com/v1/groups/{id}/roles'
+        url = f'https://groups.roblox.com/v1/groups/{str(id)}/roles'
         results = json.loads(self._request(url=url, method="GET"))
         return results
     
     def groupPayout(self, groupid, userid, amount):
-        url = f'https://groups.roblox.com/v1/groups/{groupid}/payouts'
+        url = f'https://groups.roblox.com/v1/groups/{str(groupid)}/payouts'
         data = {
             'PayoutType': 'FixedAmount',
             'Recipients': [
@@ -38,3 +38,10 @@ class Group:
         results = self._request(url=url, method='POST', data=json.dumps(data))
         return results
       
+    def postShout(self, groupid, message):
+        url = f'https://groups.roblox.com/v1/groups/{str(groupid)}/status'
+        data = {
+            'message': message
+        }
+        r = self._request(url=url, method='PATCH', data=json.dumps(data))
+        return json.loads(r)
