ebe60f384f78daf13dcfd50ccf0b64a6499dcbca
ira <38579267+iranathan@users.noreply.github.com>
2019-04-27 11:54:29 +0200

    add new request module

--- robloxapi/client.py
+++ robloxapi/client.py
@@ -1,9 +1,15 @@
+from .request import request
 from .User import User
-#from .Group import Group
+from .Group import Group
 def client(cookie=str()):
     global functions
-    functions = lambda: None
-    functions.User = User(cookie, False)
-    #functions.Group = Group(cookie, False)
-    return functions
-       
+            functions = lambda: None
+            functions.User = User(request(cookie))
+            functions.Group = Group(request(cookie))
+            return functions
+        
+
+
+
+
+
