0283b040df1ed351b47cba503855481bccbb1cad
ira <38579267+iranathan@users.noreply.github.com>
2019-05-02 19:00:43 +0200

    Added class Trade to client.py

--- robloxapi/client.py
+++ robloxapi/client.py
@@ -1,11 +1,13 @@
 from .request import request
 from .User import User
 from .Group import Group
+from .Trade import Trade
 def client(cookie=str()):
     global functions
     functions = lambda: None
     functions.User = User(request(cookie))
     functions.Group = Group(request(cookie))
+    functions.Trade = Trade(request(cookie))
     return functions
         
 
