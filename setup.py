84d7a497ea759cb77d2549ca21d6cee2c0ca0434
ira <38579267+iranathan@users.noreply.github.com>
2019-05-09 20:07:29 +0200

    added html2text

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ setup(
   keywords = ['python_roblox', 'roblox', 'robloxapi'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'requests',
-          'beautifulsoup4'
+          'beautifulsoup4',
+          'html2text'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
