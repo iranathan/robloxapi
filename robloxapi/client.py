81659fef59d2dfbea2ef4ea5aedf47574c573e08
ira <38579267+iranathan@users.noreply.github.com>
2019-05-10 20:55:01 +0200

    Update client.py

--- robloxapi/client.py
+++ robloxapi/client.py
@@ -2,7 +2,6 @@ from .utils.request import request
 from .User import User
 from .Group import Group
 from .Trade import Trade
-from .util.xcsrf import get_xcsrf as xcsrf
 class client:
     def __init__(self, cookie=''):
         self.request_client = request(cookie)
@@ -10,5 +9,4 @@ class client:
         self.User = User(self.request_client)
         self.Trade = Trade(self.request_client)
     
-    def get_xcsrf(self):
-        return xcsrf()
+
