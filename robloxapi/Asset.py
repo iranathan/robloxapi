e30ae3d3995e4ac18328aca02676ec6410ff2d89
ira <38579267+iranathan@users.noreply.github.com>
2019-05-10 21:12:42 +0200

    Update Asset.py

--- robloxapi/Asset.py
+++ robloxapi/Asset.py
@@ -1,7 +1,7 @@
 import json
 class Asset:
     def __init__(self, request):
-        self._request = request._request
+        self._request = request.request
     
     def getOutfits(self):
         url = 'https://avatar.roblox.com/v1/recent-items/all/list'
