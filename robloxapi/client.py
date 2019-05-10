7b14b8fbdbd27049297bca2b39747f268236be7c
ira <38579267+iranathan@users.noreply.github.com>
2019-05-10 21:11:10 +0200

    Update client.py

--- robloxapi/client.py
+++ robloxapi/client.py
@@ -2,11 +2,13 @@ from .utils.request import request
 from .User import User
 from .Group import Group
 from .Trade import Trade
+from .Asset import Asset
 class client:
     def __init__(self, cookie=''):
         self.request_client = request(cookie)
         self.Group = Group(self.request_client)
         self.User = User(self.request_client)
         self.Trade = Trade(self.request_client)
+        self.Asset = Asset(self.request_client)
     
 
