441dbe36756bfe4f994a81b82a60a21c277f2fbe
ira <38579267+iranathan@users.noreply.github.com>
2019-05-10 21:54:20 +0200

    Update Asset.py

--- robloxapi/Asset.py
+++ robloxapi/Asset.py
@@ -4,9 +4,25 @@ class Asset:
         self._request = request.request
     
     def getOutfits(self):
-        url = 'https://avatar.roblox.com/v1/recent-items/all/list'
+        url = 'https://avatar.roblox.com/v1/users/109503558/outfits?isEditable=true&itemsPerPage=50&page=1'
         r = self._request(url=url, method='GET')
         data = json.loads(r)
         Outfits = {}
         for item in data['data']:
-            print(item)
+            url = 'https://www.roblox.com/outfit-thumbnail/json?width=150&height=150&format=png&userOutfitId=' + str(item['id'])
+            outfit_data = json.loads(self._request(url=url, method='GET'))
+            Outfits[item['name']] = {
+                'id': item['id'],
+                'image': outfit_data['Url']
+            }
+        return Outfits
+    
+    def wearOutfit(self, OutfitId):
+        url = f'https://avatar.roblox.com/v1/outfits/{OutfitId}/wear'
+        data = self._request(url=url, method='POST')
+        return json.loads(data)
+    
+
+            
+            
+        
