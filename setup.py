4845a301ff9dcbc0ae7c6e962ed5a5574a870554
ira <38579267+iranathan@users.noreply.github.com>
2019-05-10 09:17:12 +0200

    Added new setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,26 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 setup(
-  name = 'robloxapi',         # How you named your package folder (MyLib)
-  packages = ['robloxapi'],   # Chose the same as "name"
-  version = '2.5',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'A Python wrapper for roblox',   # Give a short description about your library
-  author = 'Iranathan',                   # Type in your name
-  author_email = 'iranathan8@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['python_roblox', 'roblox', 'robloxapi'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'requests',
-          'beautifulsoup4',
-          'html2text'
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    name="robloxapi",
+    version="2.5",
+    packages=find_packages(),
+    scripts=['when_install.py'],
 
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
 
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    install_requires=[
+    'requests',
+    'beautifulsoup4',
+    'html2text'
+    ],
 
-    'Programming Language :: Python :: 3.6',
-  ],
+    author="iranathan",
+    description="A Python wrapper for roblox",
+    license="MIT",
+    keywords="https://github.com/iranathan/robloxapi",
+    url="https://github.com/iranathan/robloxapi",   
+    project_urls={
+        "Documentation": "https://github.com/iranathan/robloxapi/wiki",
+        "Source Code": "https://github.com/iranathan/robloxapi",
+    }
+
+
 )
