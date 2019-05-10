8343608782d8d1533b5aab8e77719922b35f19d6
ira <38579267+iranathan@users.noreply.github.com>
2019-05-10 09:17:48 +0200

    Added welcome script

---
+++ when_install.py
@@ -0,0 +1,9 @@
+print("""
+Hello, Thank you for using my roblox wrapper
+
+Discord: https://discord.gg/EDXNdAT
+You can use that discord server to get help with your package or too see other packages.
+
+Thank you!
+
+""")
