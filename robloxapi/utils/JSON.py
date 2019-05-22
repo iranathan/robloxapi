08288d1447dd3c62f29d40c83dc46ea081c6143e
ira <38579267+iranathan@users.noreply.github.com>
2019-05-22 13:23:37 +0200

    errors are always good.

--- robloxapi/utils/JSON.py
+++ robloxapi/utils/JSON.py
@@ -6,3 +6,11 @@ def unpack_json(json):
         return json.loads(json)
     except Exception as e:
         return json
+
+    
+def is_num(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
