f4ab02f9dca952cf565bf858e634e5d85dc03a8f
iranathan <iranathan8@gmail.com>
2019-08-01 14:57:54 +0000

    Added group class to client

--- robloxapi/client.py
+++ robloxapi/client.py
@@ -1,7 +1,9 @@
 from utils.request import *
 import user
+import group
 
 class client:
     def __init__(self, cookie=None):
         self.request = Request(cookie)
         self.User = user.User(self.request)
+        self.Group = group.Group(self.request)
